11542eb71b18743a63635e5cf7d8cd3db21e24b6
martinagui <martin.agui@gmail.com>
2021-01-20 17:04:40 -0300

    version 1.1

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-let express = require( 'express' );
-let morgan = require ('morgan');
-let app = express(); 
+const express = require( 'express' );
+const morgan = require('morgan'); //middleware application logger
+const nunjucks = require( 'nunjucks' );
 
-/*
-function logueo(req, res, next) {
-    console.log('ESTA ES LA PETICION' + ' ' +req.method + ' ' + req.originalUrl);
-    res.send('ESTA ES LA RESPUESTA')
+const app = express(); // crea una instancia de una aplicación de express
 
-    next();
-    // llamá a `next()`. Sino tu app recibirá pedidos 
-    // pero no responderá adecuadamente.
-}
+// Configurando Nunjucks
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
 
-app.use('/', logueo);
-*/
+app.use(morgan('tiny'))
+
+let tweetsDeEjemplo = [
+    { id: 1, name: "juan", content: "este es un tweeettt de juan" },
+    { id: 2, name: "carlos", content: "este es un tweeettt de carlos" },
+    { id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
+];
+
+app.get('/', function (req, res) {
+    res.render( 'index', { tweets: tweetsDeEjemplo });
+});
 
 
-app.use(morgan('tiny'));
+
+app.use(express.static('./public'))
 
 app.listen(3000, function(){
-    console.log('Servidor corriendo en el puerto 3000')
-    console.log('Servidor iniciado')
-    
+    console.log('Estas escuhando en el puerto 3000')
 });
+
+
 
